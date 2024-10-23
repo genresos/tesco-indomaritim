80988bed6e2ccd3a09e4a4eff3cff5df39166840
Rian Pambudi <akunryanpambudi@gmail.com>
2024-10-23 14:47:26 +0700

    fix route export2

--- resources/views/stisla/human-capital/employees/daily-worker-attendance.blade.php
+++ resources/views/stisla/human-capital/employees/daily-worker-attendance.blade.php
@@ -5,20 +5,20 @@
 $canAction = $canUpdate || $canDelete;
 @endphp
 <div>
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
 
 
     <a href="{{ route('employees.daily-worker.attendance-upload') }}" class="btn btn-primary">Upload Attendance</a>
-    <a href="{{ route('employees.daily-worker.attendance-test', ['fromDate' => '', 'toDate' => '']) }}" class="btn btn-success" id="exportButton">
+    <a href="{{ route('employees.daily-worker.attendance-export', ['fromDate' => '', 'toDate' => '']) }}" class="btn btn-success" id="exportButton">
         <i class="fas fa-file-export"></i> Export Attendance
     </a>
-</a>
+    </a>
 
 </div>
 @if(session('error'))
-    <script>
-        alert("{{ session('error') }}");
-    </script>
+<script>
+    alert("{{ session('error') }}");
+</script>
 @endif
 
 </br>
@@ -63,46 +63,46 @@ $canAction = $canUpdate || $canDelete;
 
 @push('scripts')
 <script>
-$(document).ready(function() {
-    var table = $('#datatable').DataTable();
+    $(document).ready(function() {
+        var table = $('#datatable').DataTable();
 
-    $.fn.dataTable.ext.search.push(
-        function(settings, data, dataIndex) {
+        $.fn.dataTable.ext.search.push(
+            function(settings, data, dataIndex) {
+                var fromDate = $('#fromDate').val();
+                var toDate = $('#toDate').val();
+                var date = new Date(data[3]); // Mengambil data dari kolom waktu
+                var formattedDate = date.toISOString().split('T')[0]; // Format ke YYYY-MM-DD
+
+                if (
+                    (fromDate === "" && toDate === "") ||
+                    (fromDate === "" && formattedDate <= toDate) ||
+                    (fromDate <= formattedDate && toDate === "") ||
+                    (fromDate <= formattedDate && formattedDate <= toDate)
+                ) {
+                    return true;
+                }
+                return false;
+            }
+        );
+
+        $('#fromDate, #toDate').on('change', function() {
+            table.draw();
+        });
+
+        $('#exportButton').on('click', function(e) {
+            e.preventDefault(); // Mencegah default action dari link
+
+            // Ambil nilai dari input tanggal
             var fromDate = $('#fromDate').val();
             var toDate = $('#toDate').val();
-            var date = new Date(data[3]); // Mengambil data dari kolom waktu
-            var formattedDate = date.toISOString().split('T')[0]; // Format ke YYYY-MM-DD
 
-            if (
-                (fromDate === "" && toDate === "") ||
-                (fromDate === "" && formattedDate <= toDate) ||
-                (fromDate <= formattedDate && toDate === "") ||
-                (fromDate <= formattedDate && formattedDate <= toDate)
-            ) {
-                return true;
-            }
-            return false;
-        }
-    );
+            // Buat URL baru dengan parameter
+            var url = "{{ route('employees.daily-worker.attendance-export', ['fromDate' => ':fromDate', 'toDate' => ':toDate']) }}";
+            url = url.replace(':fromDate', fromDate).replace(':toDate', toDate);
 
-    $('#fromDate, #toDate').on('change', function() {
-        table.draw();
+            // Redirect ke URL baru
+            window.location.href = url;
+        });
     });
-
-    $('#exportButton').on('click', function(e) {
-        e.preventDefault(); // Mencegah default action dari link
-
-        // Ambil nilai dari input tanggal
-        var fromDate = $('#fromDate').val();
-        var toDate = $('#toDate').val();
-
-        // Buat URL baru dengan parameter
-        var url = "{{ route('employees.daily-worker.attendance-test', ['fromDate' => ':fromDate', 'toDate' => ':toDate']) }}";
-        url = url.replace(':fromDate', fromDate).replace(':toDate', toDate);
-
-        // Redirect ke URL baru
-        window.location.href = url;
-    });
-});
 </script>
 @endpush
